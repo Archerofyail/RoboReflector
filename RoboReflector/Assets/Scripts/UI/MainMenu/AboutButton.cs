6b6b40a5714b8c49a86a156b867dcdb1bdd9394d
Scott Daniels <archerofyail@live.com>
2014-04-18 15:14:35 -0600

    Implemented about page in main menu

---
+++ RoboReflector/Assets/Scripts/UI/MainMenu/AboutButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AboutButton : MonoBehaviour
+{
+	public Transform aboutDialog;
+	public float bringDownTime = 1.5f;
+	public float speed = 50f;
+	void OnPress(bool isDown)
+	{
+		if (!isDown)
+		{
+			bringDownTime = BackButtonHandler.bringUpTime;
+			speed = -BackButtonHandler.speed;
+			StartCoroutine("BringDownAbout");
+			BackButtonHandler.isAboutDown = true;
+		}
+	}
+
+	IEnumerator BringDownAbout()
+	{
+		float timer = 0;
+		while (timer < bringDownTime)
+		{
+			aboutDialog.Translate(0, speed * Time.deltaTime, 0);
+			timer += Time.deltaTime;
+			yield return null;
+		}
+	}
+}
+
---
+++ RoboReflector/Assets/Scripts/UI/MainMenu/BackButtonHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BackButtonHandler : MonoBehaviour
+{
+	public static bool isAboutDown;
+	public static float bringUpTime = 0.7f;
+	public static float speed = 1200f;
+	public Transform aboutDialog;
+
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (isAboutDown)
+			{
+				StartCoroutine("HideAbout");
+			}
+			else
+			{
+				Application.Quit();
+			}
+		}
+	}
+
+	IEnumerator HideAbout()
+	{
+		float timer = 0;
+		while (timer < bringUpTime)
+		{
+			aboutDialog.Translate(0, speed * Time.deltaTime, 0);
+			timer += Time.deltaTime;
+			yield return null;
+		}
+	}
+}
+
